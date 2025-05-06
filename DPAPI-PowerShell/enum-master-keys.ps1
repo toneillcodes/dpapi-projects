b2c1e8d6158f326d2323093d70377f6ba1916867
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2025-05-05 22:27:33 -0400

    Update enum-master-keys.ps1

--- DPAPI-PowerShell/enum-master-keys.ps1
+++ DPAPI-PowerShell/enum-master-keys.ps1
@@ -1,5 +1,19 @@
 $protect_directory = $Env:AppData + "\Microsoft\Protect"
-$user_sid = dir $protect_directory | select-object -expandproperty Name
-$master_key_directory = $protect_directory + "\" + $user_sid
-$master_key_list = dir $master_key_directory | select-object -expandproperty Name
-echo $master_key_list
+#$user_sid = Get-ChildItem -Path $protect_directory -Directory | select-object -expandproperty Name
+$user_sid = [System.Security.Principal.WindowsIdentity]::GetCurrent().User.Value
+if($user_sid) {
+	$master_key_directory = $protect_directory + "\" + $user_sid
+	if($master_key_directory) {
+		echo "Master Key Directory: " $master_key_directory
+		$master_key_list = Get-ChildItem -Path $master_key_directory -Directory | select-object -expandproperty Name
+		if($master_key_list) {
+			echo "Master Key List: " $master_key_list
+		} else {
+			echo "No master keys found"
+		}
+	} else {
+		echo "ERROR: Unable to determine master key directory"
+	}
+} else {
+	echo "ERROR: Unable to determine SID"
+}
