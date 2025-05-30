d54fda07e514a5c7a4166762573048618576a57c
toneillcodes <148013535+toneillcodes@users.noreply.github.com>
2025-05-30 12:51:09 -0400

    Updating to use command line arguments

--- DPAPIBlobHunter/Program.cs
+++ DPAPIBlobHunter/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Win32;
+using System.Net.NetworkInformation;
+using Microsoft.Win32;
 
 public class DPAPIBlobHunter {
     //static byte[] magicBytes = { 1, 0, 0, 0, 208, 140, 157, 223, 1, 21, 209, 17, 140, 122, 0, 192, 79, 194, 151, 235 };  
@@ -6,18 +7,185 @@ public class DPAPIBlobHunter {
     static string magicBytesEncoded = "AQAAANCMnd8BFdERjHoAwE/Cl+s";
     static string registrySearchPath = "Software";
 
-    //static string searchPath = @"C:\dev\training\";
-    //static string searchPath = @"C:\dev\training\dpapi\tmp";
-    //static string searchPath = @"C:\Users\<user>\AppData\Roaming";
-    //static string searchPath = @"C:\Users\<user>\AppData\Local";
-    //static string searchPath = @"C:\Users\<user>\AppData\Local\Microsoft";
-    static string searchPath = @"C:\Users\<user>\AppData\Local\Microsoft\Credentials";
-
     static bool debugMode = false;
+    static bool showErrors = false;
+    static bool runFileSearch = false;
+    static bool runRegistrySearch = false;
 
-    public static void Main(string[] args) {
-        
-        Console.WriteLine("[*] Starting filesystem check routine");
+    public static int Main(string[] args)
+    {
+        bool fileProvided = false;
+        string fileParam = "";
+        bool regKeyProvided = false;
+        string regParam = "";
+
+        Console.WriteLine("[*] Running DPAPI Blob Hunter");
+
+        //  argument parsing
+        if (args.Length >= 1)
+        {
+            foreach (var arg in args)
+            {
+                if (arg.StartsWith("/file:"))
+                {
+                    Console.WriteLine("[>] File search enabled.");
+                    fileProvided = true;
+                    //  parse the option value
+                    int separatorIndex = arg.IndexOf(':');
+                    if (separatorIndex > -1)
+                    {
+                        string fileCmd = arg.Substring(0, separatorIndex).Trim();
+                        fileParam = arg.Substring(separatorIndex + 1).Trim();
+                        //Console.WriteLine($"cmd: {fileCmd}, param: {fileParam}");
+                        //result.Add(new KeyValuePair<string, string>(index, value));
+                    }
+                }
+
+                if (arg.StartsWith("/reg:"))
+                {
+                    Console.WriteLine("[>] Registry search enabled.");
+                    regKeyProvided = true;
+                    //  parse the option value
+                    int separatorIndex = arg.IndexOf(':');
+                    if (separatorIndex > -1)
+                    {
+                        string regCmd = arg.Substring(0, separatorIndex).Trim();
+                        regParam = arg.Substring(separatorIndex + 1).Trim();
+                        //Console.WriteLine($"cmd: {regCmd}, param: {regParam}");
+                        //result.Add(new KeyValuePair<string, string>(index, value));
+                    }
+                }
+            }
+
+            //  stop processing - we don't have one of our two required fields
+            if (fileProvided == false && regKeyProvided == false)
+            {
+                Console.WriteLine("ERROR: Either file path or registry path required!");
+            }
+            else
+            {
+                if (fileProvided)
+                {
+                    Console.WriteLine("[*] Starting filesystem check routine");
+                    if (debugMode)
+                    {
+                        Console.WriteLine($"[>] Filesystem Search Base: {fileParam}");
+                    }
+                    searchFilesFrom(fileParam);
+                }
+
+                if (regKeyProvided)
+                {
+                    Console.WriteLine("[*] Starting registry check routine");
+                    string registryBase = regParam.Substring(0, 4);
+                    string registryPath = regParam.Substring(5, regParam.Length - 5);
+                    if (debugMode)
+                    {
+                        Console.WriteLine($"[>] Registry Search Base: {registryBase}");
+                        Console.WriteLine($"[>] Registry Search Path: {registryPath}");
+                    }
+                    if (registryBase == "HKLM")
+                    {
+#pragma warning disable CA1416 // Validate platform compatibility
+                        RegistryKey rk = Registry.LocalMachine;
+#pragma warning restore CA1416 // Validate platform compatibility
+                        if (rk != null)
+                        {
+#pragma warning disable CA1416 // Validate platform compatibility
+                            RegistryKey? startingPoint = rk.OpenSubKey(registryPath);
+#pragma warning restore CA1416 // Validate platform compatibility
+                            if (startingPoint != null)
+                            {
+#pragma warning disable CA1416 // Validate platform compatibility
+                                Console.WriteLine("[*] Initiating search starting at: {0}", startingPoint.ToString());
+#pragma warning restore CA1416 // Validate platform compatibility
+                                searchRegistryFrom(startingPoint);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"ERROR: Unable to open registry key, null value returned for startingPoint");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("ERROR: Unable to open registry key, null value returned for rk");
+                        }
+                    }
+                    else if (registryBase == "HKCU")
+                    {
+#pragma warning disable CA1416 // Validate platform compatibility
+                        RegistryKey rk = Registry.CurrentUser;
+#pragma warning restore CA1416 // Validate platform compatibility
+                        if (rk != null)
+                        {
+#pragma warning disable CA1416 // Validate platform compatibility
+                            RegistryKey? startingPoint = rk.OpenSubKey(registryPath);
+#pragma warning restore CA1416 // Validate platform compatibility
+                            if (startingPoint != null)
+                            {
+#pragma warning disable CA1416 // Validate platform compatibility
+                                Console.WriteLine("[*] Initiating search starting at: {0}", startingPoint.ToString());
+#pragma warning restore CA1416 // Validate platform compatibility
+                                searchRegistryFrom(startingPoint);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"ERROR: Unable to open registry key, null value returned for startingPoint");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("ERROR: Unable to open registry key, null value returned for rk");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("ERROR: Unrecognized registry base, valid values: HKLM or HKCU");
+                        System.Environment.Exit(-1);
+                    }
+                    Console.WriteLine("[*] Registry processing completed.");
+                }
+            }
+        }
+        else
+        {
+            Console.WriteLine("ERROR: Either file path or registry path required!");
+        }
+
+        Console.WriteLine("[*] Done.");
+
+        return 0;
+    }
+
+    public static bool ContainsBytes(byte[] source, byte[] pattern)
+    {
+        // check the parameters
+        if (source == null || pattern == null || pattern.Length == 0 || source.Length < pattern.Length)
+        {
+            return false;
+        }
+
+        for (int i = 0; i <= source.Length - pattern.Length; i++)
+        {
+            bool found = true;
+            for (int j = 0; j < pattern.Length; j++)
+            {
+                if (source[i + j] != pattern[j])
+                {
+                    found = false;
+                    break;
+                }
+            }
+            if (found)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void searchFilesFrom(string searchPath)
+    {
         try
         {
             // https://learn.microsoft.com/en-us/dotnet/api/system.io.directory?view=net-9.0
@@ -27,16 +195,16 @@ public class DPAPIBlobHunter {
             {
                 string fileName = currentFile.Substring(searchPath.Length + 1);
                 if(debugMode) {
-                    Console.WriteLine("DEBUG Processing: {0}", fileName);
+                    Console.WriteLine("[DEBUG] Processing: {0}", fileName);
                 }
                 // FileStream byte processing based on the FileStream.Read Method reference
                 try {
                     using (FileStream fs = File.OpenRead(currentFile)) {
                         if(fs.Length > Int32.MaxValue) {
-                            Console.WriteLine("File too large, skipping {0}",currentFile);
+                            Console.WriteLine("[>] File too large, skipping {0}",currentFile);
                         } else {
                             if(debugMode) {
-                                Console.WriteLine("DEBUG fs.length: {0}",fs.Length);
+                                Console.WriteLine("[DEBUG] fs.length: {0}",fs.Length);
                             }
                             //byte[] inputBytes = new byte[20];
                             byte[] inputBytes = new byte[fs.Length];
@@ -66,7 +234,7 @@ public class DPAPIBlobHunter {
                             // now we need to compare the bytes in inputBytes to magicBytes
                             bool comparisonResult = ContainsBytes(inputBytes,magicBytes);
                             if(debugMode) {
-                                Console.WriteLine("DEBUG: comparisonResult: {0}", comparisonResult);
+                                Console.WriteLine("[DEBUG] comparisonResult: {0}", comparisonResult);
                             }
 
                             if(comparisonResult) {
@@ -83,88 +251,67 @@ public class DPAPIBlobHunter {
                     }
                 } catch (IOException e) {
                     // ignore the exception, probably inaccessible file
-                    if(debugMode) {
-                        Console.WriteLine("IO ERROR: Exception: {0}", e.Message);  
+                    if(showErrors || debugMode) {
+                        Console.WriteLine("[ERROR] IO Exception: {0}", e.Message);  
                     }
                 }  catch (Exception e) {
-                    if(debugMode) {
-                        Console.WriteLine("ERROR: Exception: {0}", e.Message);
+                    if(showErrors || debugMode) {
+                        Console.WriteLine("[ERROR] Exception: {0}", e.Message);
                     }
                 }               
             }
-        } catch (IOException e) {
+        /*} catch (IOException e) {
             // ignore the exception, probably inaccessible file
             if(debugMode) {
                 Console.WriteLine("IO ERROR: Exception: {0}", e.Message);  
-            }
+            }*/
         } catch (Exception e) {
-            if(debugMode) {
-                Console.WriteLine("ERROR: Exception: {0}", e.Message);
+            if(showErrors || debugMode) {
+                Console.WriteLine("[ERROR] Exception unable to enumerate file search base: {0}", e.Message);
             }
         }
 
         Console.WriteLine("[*] Filesystem processing completed.");
-
-        Console.WriteLine("[*] Starting registry check routine");
-
-        //RegistryKey rk = Registry.LocalMachine;
-        RegistryKey rk = Registry.CurrentUser;
-        RegistryKey startingPoint = rk.OpenSubKey(registrySearchPath);
-
-        Console.WriteLine("[*] Initiating search starting at: {0}", startingPoint.ToString());
-
-        searchRegistryFrom(startingPoint);
-
-        Console.WriteLine("[*] Registry processing completed.");
-
-        Console.WriteLine("[*] Done.");
     }
 
-    public static bool ContainsBytes(byte[] source, byte[] pattern) {
-        // check the parameters
-        if (source == null || pattern == null || pattern.Length == 0 || source.Length < pattern.Length)
-        {
-            return false; 
-        }
-
-        for (int i = 0; i <= source.Length - pattern.Length; i++)
-        {
-            bool found = true;
-            for (int j = 0; j < pattern.Length; j++)
-            {
-                if (source[i + j] != pattern[j])
-                {
-                    found = false;
-                    break;
-                }
-            }
-            if (found)
-            {
-                return true; 
-            }
-        }
-        return false;
-    }
-    
-    public static void searchRegistryFrom(RegistryKey startingPoint) {
+    public static void searchRegistryFrom(RegistryKey startingPoint)
+    {
         // we have a starting point, now we need to parse all values
-        foreach(string valueName in startingPoint.GetValueNames()) {
+#pragma warning disable CA1416 // Validate platform compatibility
+        foreach (string valueName in startingPoint.GetValueNames())
+        {
+#pragma warning disable CA1416 // Validate platform compatibility
             string tempValue = startingPoint.GetValue(valueName).ToString();
-            if(tempValue.Length >= 28) {
-                if(tempValue.Contains(magicBytesEncoded)) {
+#pragma warning restore CA1416 // Validate platform compatibility
+            if (tempValue.Length >= 28)
+            {
+                if (tempValue.Contains(magicBytesEncoded))
+                {
+#pragma warning disable CA1416 // Validate platform compatibility
                     Console.WriteLine("[!] Probable registry blob found at: {0}\\{1}", startingPoint.ToString(), valueName);
+#pragma warning restore CA1416 // Validate platform compatibility
                 }
-            }  else {
-                if(debugMode) {
-                    Console.WriteLine("DEBUG: Value too short, skipping");
+            }
+            else
+            {
+                if (debugMode)
+                {
+                    Console.WriteLine("[DEBUG] Value too short, skipping");
                 }
             }
         }
+#pragma warning restore CA1416 // Validate platform compatibility
         // recursive execution to process all subkeys
-        foreach(string subKeyName in startingPoint.GetSubKeyNames()) {
-            using(RegistryKey tempKey = startingPoint.OpenSubKey(subKeyName)) {
-                searchRegistryFrom(tempKey);    
+#pragma warning disable CA1416 // Validate platform compatibility
+        foreach (string subKeyName in startingPoint.GetSubKeyNames())
+        {
+#pragma warning disable CA1416 // Validate platform compatibility
+            using (RegistryKey tempKey = startingPoint.OpenSubKey(subKeyName))
+            {
+                searchRegistryFrom(tempKey);
             }
-        }            
+#pragma warning restore CA1416 // Validate platform compatibility
+        }
+#pragma warning restore CA1416 // Validate platform compatibility
     }
 }
